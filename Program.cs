23f3ccb212427137b1e57ebf0bb62e8aae69a578
unknown <Swetha>
2024-02-16 13:27:35 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+
+Calc calc = new Calc();
+Console.WriteLine("Add Call");
+calc.Add(12, 45);
+Console.WriteLine("Multiply Call");
+calc.Multi(10.5, 12.45);
+Console.WriteLine("Division Call");
+calc.Div(20, 4);
+Console.WriteLine("Sub Call");
+calc.Sub(20, 4);
